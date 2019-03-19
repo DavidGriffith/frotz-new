8da3feb617e6eb34347284236bb56b17b93164c5
David Griffith <dave@661.org>
2019-03-18 20:45:13 -0700

    Adding X11 specific code straight from xfrotz 2.32.1.

---
+++ src/x11/x_frotz.h
@@ -0,0 +1,34 @@
+/*
+ * x_frotz.h
+ *
+ * X interface, declarations
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <X11/Xlib.h>
+
+/* X connection */
+extern Display *dpy;
+
+/* Window to draw into */
+extern Window main_window;
+
+extern char *x_class, *x_name;
+
+/* The font resource for displaying text */
+extern const XFontStruct *current_font_info;
+extern GC current_gc, normal_gc, reversed_gc, bw_gc;
+extern char *font_names[9];
+
+extern int curr_x, curr_y;
+
+extern unsigned long bg_pixel, fg_pixel;
+extern unsigned long def_bg_pixel, def_fg_pixel;
+
+const XFontStruct * get_font(int font, int style);
+
+void x_init_colour(char *bg_name, char *fg_name);
