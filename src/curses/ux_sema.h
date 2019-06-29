85ccd9147c36bdafa71976b1066d2e7a3aa402c1
Bill Lash <william.lash@gmail.com>
2019-06-28 19:23:52 -0500

    Update license on ux_sema.h

--- src/curses/ux_sema.h
+++ src/curses/ux_sema.h
@@ -17,6 +17,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  * Or visit http://www.fsf.org/
+ *
+ * This file and only this file is dual licensed under the MIT license.
+ *
  */
 
 /*
