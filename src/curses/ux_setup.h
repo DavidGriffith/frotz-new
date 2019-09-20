c2adcef93a61bfba9312e38645a891b192cb9ebb
David Griffith <dave@661.org>
2019-09-19 19:24:23 -0700

    Convert ux_setup.h to K&R style.

--- src/curses/ux_setup.h
+++ src/curses/ux_setup.h
@@ -8,8 +8,8 @@ typedef struct unix_setup_struct {
 	int random_seed;
 	int tandy_bit;
 	int current_text_style;		/* also in ux_text.c and ux_screen.c */
-        int curses_active;
-        int plain_ascii;
+	int curses_active;
+	int plain_ascii;
 	int current_color;		/* ux_text.c ux_screen.c */
 	bool color_enabled;		/* ux_init.c ux_pic.c ux_text.c */
 	int interpreter;		/* see frotz.h */
