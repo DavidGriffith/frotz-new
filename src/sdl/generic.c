352e0e5e01b88a0259d555ee7eee4c7b14adf6c9
David Griffith <dave@661.org>
2019-10-01 00:00:35 -0700

    Add check for explicitly-set blorb filename.

--- src/sdl/generic.c
+++ src/sdl/generic.c
@@ -81,16 +81,19 @@ bb_err_t gen_blorb_init(char *filename)
 		blorb_fp = fp;
 	} else {
 		fclose(fp);
-		len1 = strlen(filename) + strlen(EXT_BLORB);
-		len2 = strlen(filename) + strlen(EXT_BLORB3);
 
-		mystring = malloc(len2 * sizeof(char) + 1);
-		strncpy(mystring, filename, len1 * sizeof(char));
-		p = strrchr(mystring, '.');
-		if (p != NULL)
-			*p = '\0';
-
-		strncat(mystring, EXT_BLORB, len1 * sizeof(char));
+		if (f_setup.blorb_file != NULL)
+			mystring = strdup(f_setup.blorb_file);
+		else {
+			len1 = strlen(filename) + strlen(EXT_BLORB);
+			len2 = strlen(filename) + strlen(EXT_BLORB3);
+			mystring = malloc(len2 * sizeof(char) + 1);
+			strncpy(mystring, filename, len1 * sizeof(char));
+			p = strrchr(mystring, '.');
+			if (p != NULL)
+				*p = '\0';
+			strncat(mystring, EXT_BLORB, len1 * sizeof(char));
+		}
 
 		/* Check if foo.blb is there. */
 		if ((fp = fopen(mystring, "rb")) == NULL) {
