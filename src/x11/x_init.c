0cc005efdf0e7190f5f8582a65021be77bbab984
Bill Lash <william.lash@gmail.com>
2020-01-11 21:52:47 -0600

    Add default file names
    Fixes crash when saving a game

--- src/x11/x_init.c
+++ src/x11/x_init.c
@@ -217,6 +217,7 @@ void os_process_arguments (int argc, char *argv[])
   XtAppContext app_context;
   char *str_type_return;
   char *class_buf, *name_buf, *class_append, *name_append;
+  char *p;
   int i;
   XrmValue value;
 
@@ -233,6 +234,34 @@ void os_process_arguments (int argc, char *argv[])
 
   f_setup.story_file = strdup(argv[1]);
   f_setup.story_name = strdup(basename(argv[1]));
+
+  /* Now strip off the extension */
+  p = strrchr(f_setup.story_name, '.');
+  if ( p != NULL )
+    *p = '\0';  /* extension removed */
+
+  /* Create nice default file names */
+  f_setup.script_name = malloc((strlen(f_setup.story_name) + strlen(EXT_SCRIPT)) * sizeof(char) + 1);
+  memcpy(f_setup.script_name, f_setup.story_name, (strlen(f_setup.story_name) + strlen(EXT_SCRIPT)) * sizeof(char));
+  strncat(f_setup.script_name, EXT_SCRIPT, strlen(EXT_SCRIPT) + 1);
+
+  f_setup.command_name = malloc((strlen(f_setup.story_name) + strlen(EXT_COMMAND)) * sizeof(char) + 1);
+  memcpy(f_setup.command_name, f_setup.story_name, (strlen(f_setup.story_name) + strlen(EXT_COMMAND)) * sizeof(char));
+  strncat(f_setup.command_name, EXT_COMMAND, strlen(EXT_COMMAND) + 1);
+
+  if (!f_setup.restore_mode) {
+    f_setup.save_name = malloc((strlen(f_setup.story_name) + strlen(EXT_SAVE)) * sizeof(char) + 1);
+    memcpy(f_setup.save_name, f_setup.story_name, (strlen(f_setup.story_name) + strlen(EXT_SAVE)) * sizeof(char));
+    strncat(f_setup.save_name, EXT_SAVE, strlen(EXT_SAVE) + 1);
+  } else {  /*Set our auto load save as the name_save*/
+    f_setup.save_name = malloc((strlen(f_setup.tmp_save_name) + strlen(EXT_SAVE)) * sizeof(char) + 1);
+    memcpy(f_setup.save_name, f_setup.tmp_save_name, (strlen(f_setup.tmp_save_name) + strlen(EXT_SAVE)) * sizeof(char));
+    free(f_setup.tmp_save_name);
+  }
+
+  f_setup.aux_name = malloc((strlen(f_setup.story_name) + strlen(EXT_AUX)) * sizeof(char) + 1);
+  memcpy(f_setup.aux_name, f_setup.story_name, (strlen(f_setup.story_name) + strlen(EXT_AUX)) * sizeof(char));
+  strncat(f_setup.aux_name, EXT_AUX, strlen(EXT_AUX) + 1);
 
   XtGetApplicationNameAndClass(dpy, &x_name, &x_class);
 
