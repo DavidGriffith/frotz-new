25b899cf9f68f5579b9b98716a16492edbe03033
David Griffith <dave@661.org>
2020-01-12 05:14:41 -0800

    Convert x_screen.c to K&R style.

--- src/x11/x_screen.c
+++ src/x11/x_screen.c
@@ -10,6 +10,7 @@
 #include "x_frotz.h"
 #include <stdio.h>
 
+
 /*
  * os_erase_area
  *
@@ -17,12 +18,12 @@
  * colour. Top left coordinates are (1,1). The cursor does not move.
  *
  */
-
-void os_erase_area (int top, int left, int bottom, int right, int UNUSED(win))
+void os_erase_area(int top, int left, int bottom, int right, int UNUSED(win))
 {
-  XFillRectangle(dpy, main_window, reversed_gc,
-                 left - 1, top - 1, right - left + 1, bottom - top + 1);
-}/* os_erase_area */
+	XFillRectangle(dpy, main_window, reversed_gc,
+		       left - 1, top - 1, right - left + 1, bottom - top + 1);
+} /* os_erase_area */
+
 
 /*
  * os_scroll_area
@@ -32,34 +33,34 @@ void os_erase_area (int top, int left, int bottom, int right, int UNUSED(win))
  * colour. Top left coordinates are (1,1). The cursor stays put.
  *
  */
-
-void os_scroll_area (int top, int left, int bottom, int right, int units)
+void os_scroll_area(int top, int left, int bottom, int right, int units)
 {
-  if (units == 0)
-    return;
-  else if (units > bottom - top || units < top - bottom)
-    XFillRectangle(dpy, main_window, reversed_gc,
-                   left - 1, top - 1, right - left + 1, bottom - top + 1);
-  else if (units > 0) {
-    XCopyArea(dpy, main_window, main_window, current_gc,
-              left - 1, top - 1 + units,
-              right - left + 1, bottom - top - units + 1,
-              left - 1, top - 1);
-    XFillRectangle(dpy, main_window, reversed_gc,
-                   left - 1, bottom - units, right - left + 1, units);
-  }
-  else {
-    XCopyArea(dpy, main_window, main_window, current_gc,
-              left - 1, top - 1, right - left + 1, bottom - top + units + 1,
-              left - 1, top - 1 + units);
-    XFillRectangle(dpy, main_window, reversed_gc,
-                   left - 1, top - 1, right - left + 1, - units);
-  }
-}/* os_scroll_area */
+	if (units == 0)
+		return;
+	else if (units > bottom - top || units < top - bottom)
+		XFillRectangle(dpy, main_window, reversed_gc,
+			       left - 1, top - 1, right - left + 1,
+			       bottom - top + 1);
+	else if (units > 0) {
+		XCopyArea(dpy, main_window, main_window, current_gc,
+			  left - 1, top - 1 + units,
+			  right - left + 1, bottom - top - units + 1,
+			  left - 1, top - 1);
+		XFillRectangle(dpy, main_window, reversed_gc,
+			       left - 1, bottom - units, right - left + 1,
+			       units);
+	} else {
+		XCopyArea(dpy, main_window, main_window, current_gc,
+			  left - 1, top - 1, right - left + 1,
+			  bottom - top + units + 1, left - 1, top - 1 + units);
+		XFillRectangle(dpy, main_window, reversed_gc, left - 1, top - 1,
+			       right - left + 1, -units);
+	}
+} /* os_scroll_area */
 
 
 bool os_repaint_window(int win, int ypos_old, int ypos_new, int xpos,
-                       int ysize, int xsize)
+		       int ysize, int xsize)
 {
-    return TRUE;
+	return TRUE;
 }
