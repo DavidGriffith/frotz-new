50ce135fe43eb19d6f09baf28494a10b926bc8dc
David Griffith <dave@661.org>
2020-08-12 17:22:23 -0700

    Add os_from_true_colour() and os_to_true_colour() to X11 interface.

--- src/x11/x_screen.c
+++ src/x11/x_screen.c
@@ -64,3 +64,68 @@ bool os_repaint_window(int win, int ypos_old, int ypos_new, int xpos,
 {
 	return TRUE;
 }
+
+
+/*
+ * os_from_true_colour
+ *
+ * Given a true colour, return an appropriate colour index.
+ *
+ * This is pretty brain-dead, and just makes it 3-bit true-color first.
+ * Eventually this will be changed to how sfrotz does it.
+ *
+ */
+int os_from_true_colour(zword colour)
+{
+	if (colour == 0xfffe)
+		return 0;
+	else if (colour == 0xffff)
+		return 1;
+	else {
+		int r = colour & 0x001F;
+		int g = colour & 0x03E0;
+		int b = colour & 0x7C00;
+		int index = (r ? 4 : 0) | (g ? 2 : 0) | (b ? 1 : 0);
+
+		switch (index) {
+		case 0: return 2;
+		case 1: return 6;
+		case 2: return 4;
+		case 3: return 8;
+		case 4: return 3;
+		case 5: return 7;
+		case 6: return 5;
+		case 7: return 9;
+		default: return 1; /* Can't happen */
+		}
+	}
+}
+
+
+/*
+ * os_to_true_colour
+ *
+ * Given a colour index, return the appropriate true colour.
+ *
+ * Eventually this will be changed to how sfrotz does it.
+ *
+ */
+zword os_to_true_colour(int index)
+{
+	switch (index) {
+	case 0: return -2;
+	case 1: return -1;
+	case 2: return 0x0000;
+	case 3: return 0x001D;
+	case 4: return 0x0340;
+	case 5: return 0x03BD;
+	case 6: return 0x59A0;
+	case 7: return 0x7C1F;
+	case 8: return 0x77A0;
+	case 9: return 0x7FFF;
+	case 10: return 0x5AD6;
+	case 11: return 0x4631;
+	case 12: return 0x2D6B;
+	default: return 0x0000;
+	}
+}
