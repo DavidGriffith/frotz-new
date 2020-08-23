76ae5f67c3e61b3197367c633c48aeff03bd1bec
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2020-08-23 23:02:45 +0800

    Open Watcom: port owinput.c from Borland C version

--- src/owdos/owfrotz.h
+++ src/owdos/owfrotz.h
@@ -127,4 +127,36 @@ extern volatile int end_of_sound_flag;
 /* owtext  */	void	switch_scrn_attr (bool);
 /* owtext  */	void 	load_fonts (void);
 
+/*
+ * Inline functions for calling BIOS video and date/time services, and DOS
+ * mouse services.
+ */
+word bios_video_ah(byte ah);
+#pragma aux bios_video_ah = "int 0x10" parm [ah] value [ax] modify [bx cx dx];
+word bios_video_ah_bh_dh_dl(byte ah, byte bh, byte dh, byte dl);
+#pragma aux bios_video_ah_bh_dh_dl = "int 0x10" parm [ah] [bh] [dh] [dl] \
+				     value [ax] modify [bx cx dx];
+word bios_video_ah_cx(byte ah, word cx);
+#pragma aux bios_video_ah_cx = "int 0x10" parm [ah] [cx] value [ax] \
+			       modify [bx cx dx];
+word bios_video_ax(word ax);
+#pragma aux bios_video_ax = "int 0x10" parm [ax] value [ax] modify [bx cx dx];
+word bios_video_ax_bl(word ax, byte bl);
+#pragma aux bios_video_ax_bl = "int 0x10" parm [ax] [bl] value [ax] \
+			       modify [bx cx dx];
+word bios_video_ax_bx(word ax, word bx);
+#pragma aux bios_video_ax_bx = "int 0x10" parm [ax] [bx] value [ax] \
+			       modify [bx cx dx];
+word bios_video_ax_bx_dh_ch_cl(word ax, word bx, byte dh, byte ch, byte cl);
+#pragma aux bios_video_ax_bx_dh_ch_cl = "int 0x10" \
+					parm [ax] [bx] [dh] [ch] [cl] \
+					value [ax] modify [bx cx dx];
+word bios_video_ax_esdx_bh(word ax, void _far *esdx, byte bh);
+#pragma aux bios_video_ax_esdx_bh = "int 0x10" parm [ax] [es dx] [bh] \
+				    value [ax] modify [bx cx dx];
+long bios_time_ah(byte ah);
+#pragma aux bios_time_ah = "int 0x1a" parm [ah] value [cx dx] modify [ax bx];
+void dos_mouse_ax(word ax);
+#pragma aux dos_mouse_ax = "int 0x33" parm [ax] modify [ax bx cx dx];
+
 #endif
--- src/owdos/owinit.c
+++ src/owdos/owinit.c
@@ -205,33 +205,6 @@ char *basename(const char *path)
 
 
 /*
- * Inline functions for calling BIOS video and date/time services.
- */
-word bios_video_ah(byte ah);
-#pragma aux bios_video_ah = "int 0x10" parm [ah] value [ax] modify [bx cx dx];
-word bios_video_ah_cx(byte ah, word cx);
-#pragma aux bios_video_ah_cx = "int 0x10" parm [ah] [cx] value [ax] \
-			       modify [bx cx dx];
-word bios_video_ax(word ax);
-#pragma aux bios_video_ax = "int 0x10" parm [ax] value [ax] modify [bx cx dx];
-word bios_video_ax_bl(word ax, byte bl);
-#pragma aux bios_video_ax_bl = "int 0x10" parm [ax] [bl] value [ax] \
-			       modify [bx cx dx];
-word bios_video_ax_bx(word ax, word bx);
-#pragma aux bios_video_ax_bx = "int 0x10" parm [ax] [bx] value [ax] \
-			       modify [bx cx dx];
-word bios_video_ax_bx_dh_ch_cl(word ax, word bx, byte dh, byte ch, byte cl);
-#pragma aux bios_video_ax_bx_dh_ch_cl = "int 0x10" \
-					parm [ax] [bx] [dh] [ch] [cl] \
-					value [ax] modify [bx cx dx];
-word bios_video_ax_esdx_bh(word ax, void _far *esdx, byte bh);
-#pragma aux bios_video_ax_esdx_bh = "int 0x10" parm [ax] [es dx] [bh] \
-				    value [ax] modify [bx cx dx];
-long bios_time_ah(byte ah);
-#pragma aux bios_time_ah = "int 0x1a" parm [ah] value [cx dx] modify [ax bx];
-
-
-/*
  * cleanup
  *
  * Shut down the IO interface: free memory, close files, restore
