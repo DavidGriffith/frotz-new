7e92e922f3dd4b7914196f0e252fc2cd40a9268b
David Griffith <dave@661.org>
2023-05-01 08:28:13 -0700

    Updated xfrotz's usage info.

--- src/x11/x_info.h
+++ src/x11/x_info.h
@@ -9,17 +9,17 @@
 #define X_INFO_H
 
 #define INFORMATION "\
-  -aa   watch attribute setting   \t -rm # right margin\n\
-  -at   watch attribute testing   \t -rs # random number seed value\n\
-  -bg <colorname> background color\t -sc # transcript width\n\
-  -c # context lines              \t -t   set Tandy bit\n\
-  -fg <colorname> foreground color\t -u # slots for multiple undo\n\
-  -fn <fontname> set font name    \t -v   show version information\n\
-  -i   ignore fatal errors        \t -w # text width\n\
-  -lm # left margin               \t -x   expand abbreviations g/x/z\n\
-  -ol   watch object movement     \t -xrm <resources> Set X11 resources\n\
-  -om   watch object locating     \t -zs # error checking (see below)\n\
-  -P   alter piracy opcode\n"
+  -aa   watch attribute setting   \t -P   alter piracy opcode\n\
+  -at   watch attribute testing   \t -rm # right margin\n\
+  -bg <colorname> background color\t -rs # random number seed value\n\
+  -c # context lines              \t -sc # transcript width\n\
+  -fg <colorname> foreground color\t -t   set Tandy bit\n\
+  -fn <fontname> set font name    \t -u # slots for multiple undo\n\
+  -i   ignore fatal errors        \t -v   show version information\n\
+  -lm # left margin               \t -w # text width\n\
+  -ol   watch object movement     \t -x   expand abbreviations g/x/z\n\
+  -L  <file> load this save file  \t -xrm <resources> Set X11 resources\n\
+  -om   watch object locating     \t -zs # error checking (see below)\n"
 
 #define INFO2 "\
 Error checking: 0 none, 1 first only (default), 2 all, 3 exit after any error.\n\
