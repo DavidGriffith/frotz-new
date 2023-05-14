DavidGriffith/frotz-new

commit 6c65ac8eef2e98e801dbc358e7068d834aa269a2

Added save-restore prompt option to dumb interface with restriction to .aux.

src/dumb/dinput.c
@@ -529,6 +529,7 @@ char *os_read_file_name (const char *default_name, int flag)
 	char *tempname;
 	char path_separator[2];
 	int i;
+	char *ext;
 
 	path_separator[0] = PATH_SEPARATOR;
 	path_separator[1] = 0;
@@ -538,6 +539,13 @@ char *os_read_file_name (const char *default_name, int flag)
 	 */
 	if (f_setup.restore_mode) {
 		return strdup(default_name);
+	} else if (flag == FILE_NO_PROMPT) {
+		ext = strrchr(default_name, '.');
+		if (strncmp(ext, EXT_AUX, 4)) {
+			os_warn("Blocked unprompted access of %s. Should only be %s files.", default_name, EXT_AUX);
+			return NULL;
+		}
+		buf = strndup(default_name, MAX_FILE_NAME);
 	} else {
 		if (f_setup.restricted_path) {
 			for (i = strlen(default_name); i > 0; i--) {
@@ -561,10 +569,10 @@ char *os_read_file_name (const char *default_name, int flag)
 		}
 	}
 
-	if (buf[0])
-		strncpy(file_name, fullpath, FILENAME_MAX);
-	else
+	if (buf == NULL || flag == FILE_NO_PROMPT)
 		strncpy(file_name, default_name, FILENAME_MAX);
+	else
+		strncpy(file_name, fullpath, FILENAME_MAX);
 
 	/* Check if we're restricted to one directory. */
 	if (f_setup.restricted_path != NULL) {

src/dumb/dinit.c
@@ -19,6 +19,8 @@
  * Or visit http://www.fsf.org/
  */
 
+#include <stdarg.h>
+
 #include "dfrotz.h"
 #include "dblorb.h"
 
@@ -315,6 +317,37 @@ void os_quit(int status)
 void os_restart_game (int UNUSED (stage)) {}
 
 
+/*
+ * os_warn
+ *
+ * Display a warning message and continue with the game.
+ *
+ */
+void os_warn (const char *s, ...)
+{
+	va_list m;
+	int style;
+
+	os_beep(BEEP_HIGH);
+	style = os_get_text_style();
+	os_set_text_style(BOLDFACE_STYLE);
+	fprintf(stderr, "Warning: ");
+	os_set_text_style(NORMAL_STYLE);
+	va_start(m, s);
+	vfprintf(stderr, s, m);
+	va_end(m);
+	fprintf(stderr, "\n");
+	os_set_text_style(style);
+	return;
+}
+
+
+/*
+ * os_fatal
+ *
+ * Display error message and exit program.
+ *
+ */
 void os_fatal (const char *s, ...)
 {
 	fprintf(stderr, "\nFatal error: %s\n", s);
@@ -322,7 +355,7 @@ void os_fatal (const char *s, ...)
 		fprintf(stderr, "Continuing anyway...\n");
 	else
 		os_quit(EXIT_FAILURE);
-}
+} /* os_fatal */
 
 
 FILE *os_load_story(void)

src/dumb/doutput.c
@@ -718,6 +718,34 @@ bool os_repaint_window(int UNUSED(win), int UNUSED(ypos_old),
 }
 
 
+/*
+ * os_get_text_style
+ *
+ * Return the current text style.  Following flags can be set:
+ *
+ *     REVERSE_STYLE
+ *     BOLDFACE_STYLE
+ *     EMPHASIS_STYLE (aka underline aka italics)
+ *     FIXED_WIDTH_STYLE
+ *
+ */
+int os_get_text_style(void)
+{
+	return current_style;
+}
+
+
+/*
+ * os_set_text_style
+ *
+ * Set the current text style.  Following flags can be set:
+ *
+ *     REVERSE_STYLE
+ *     BOLDFACE_STYLE
+ *     EMPHASIS_STYLE (aka underline aka italics)
+ *     FIXED_WIDTH_STYLE
+ *
+ */
 void os_set_text_style(int x)
 {
 	current_style = x;

commit 421a7a93db5fb2e23748ace1ffa14f4c3dca0159

Fixed game save in dfrotz broken in 6c65ac8e.

src/dumb/dinput.c
@@ -551,32 +551,26 @@ char *os_read_file_name (const char *default_name, int flag)
 		else
 			buf = basename((char *)default_name);
 	} else {
-		if (f_setup.restricted_path) {
-			for (i = strlen(default_name); i > 0; i--) {
-				if (default_name[i] == PATH_SEPARATOR) {
-					i++;
-					break;
-				}
-			}
-			tempname = strdup(default_name + i);
-			sprintf(prompt, "Please enter a filename [%s]: ", tempname);
+		if (f_setup.restricted_path != NULL) {
+			sprintf(prompt, "Please enter a filename [%s]: ", basename((char *)default_name));
 		} else
 			sprintf(prompt, "Please enter a filename [%s]: ", default_name);
+
 		dumb_read_misc_line(fullpath, prompt);
-		if (!fullpath[0])
-			buf = fullpath;
-		else
-			buf = basename(fullpath);
+
+		/* If using default filename... */
+		if (strlen(fullpath) == 0)
+			buf = strndup(default_name, MAX_FILE_NAME);
+		else /* Using supplied filename... */
+			buf = strndup(fullpath, MAX_FILE_NAME);
+
 		if (strlen(buf) > MAX_FILE_NAME) {
 			printf("Filename too long\n");
 			return NULL;
 		}
 	}
 
-	if (buf == NULL || flag == FILE_NO_PROMPT)
-		strncpy(file_name, default_name, FILENAME_MAX);
-	else
-		strncpy(file_name, fullpath, FILENAME_MAX);
+	strncpy(file_name, buf, FILENAME_MAX);
 
 	/* Check if we're restricted to one directory. */
 	if (f_setup.restricted_path != NULL) {